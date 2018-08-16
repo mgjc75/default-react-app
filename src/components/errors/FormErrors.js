4b328fbcb7ae6ae20536838b8a8314b094fe582f
Mark Campbell <markcampbell@Marks-MBP.fritz.box>
2018-08-16 12:54:15 +0100

    started on form validation for adding a trip

---
+++ src/components/errors/FormErrors.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class FormErrors extends Component {
+  render() {
+    const { formErrors } = this.props;
+    return (
+      <div className="formErrors">
+        {Object.keys(formErrors).map((fieldName, i) => {
+          if (formErrors[fieldName].length > 0) {
+            return (
+              <p key={i}>
+                {fieldName} {formErrors[fieldName]}
+              </p>
+            );
+          } else {
+            return "";
+          }
+        })}
+      </div>
+    );
+  }
+}
+
+export default FormErrors;
--- src/components/forms/AddTrip.js
+++ src/components/forms/AddTrip.js
@@ -1,96 +1,192 @@
 import React, { Component } from "react";
+import FormErrors from "../errors/FormErrors";
 
 class componentName extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: "",
+      ppl: "",
+      cost: "",
+      litres: "",
+      mileage: "",
+      trip: "",
+      formErrors: {
+        date: "",
+        ppl: "",
+        cost: "",
+        litres: "",
+        mileage: "",
+        trip: ""
+      },
+      formValid: false
+    };
+  }
+
   render() {
     return (
-      <form>
-        <div className="form-row p-2">
-          <div className="input-group">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="inputGroupPrepend2">
-                Date
-              </span>
+      <div className="row">
+        <div className="col-md-8">
+          <form>
+            <div className="form-row p-2">
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroupDate">
+                    Date
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="validationDate"
+                  placeholder="date"
+                  aria-describedby="inputGroupDate"
+                  required
+                  value={this.state.date}
+                  onChange={event => this.handleUserInput(event)}
+                />
+              </div>
             </div>
-            <input
-              type="text"
-              className="form-control"
-              id="validationDefaultUsername"
-              placeholder="date"
-              aria-describedby="inputGroupPrepend2"
-              required
-            />
+            <div className="form-row p-2">
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroupPpl">
+                    £/ppl
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="validationPpl"
+                  placeholder="£ Pence Per Litre"
+                  aria-describedby="inputGroupPpl"
+                  required
+                  value={this.state.ppl}
+                />
+              </div>
+            </div>
+            <div className="form-row p-2">
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputLitres">
+                    L
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="validationLitres"
+                  placeholder="litres"
+                  aria-describedby="inputLitres"
+                  required
+                  value={this.state.litres}
+                />
+              </div>
+            </div>
+            <div className="form-row p-2">
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroupCost">
+                    £
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="validationCost"
+                  placeholder="£"
+                  aria-describedby="inputGroupCost"
+                  required
+                  value={this.state.cost}
+                />
+              </div>
+            </div>
+            <div className="form-row p-2">
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroupMileage">
+                    mileage
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="validationMileage"
+                  placeholder="mileage"
+                  aria-describedby="inputGroupMileage"
+                  required
+                  value={this.state.mileage}
+                />
+              </div>
+            </div>
+            <div className="form-row p-2">
+              <div className="input-group">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="inputGroupTrip">
+                    Trip
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="validationTrip"
+                  placeholder="trip"
+                  aria-describedby="inputGroupTrip"
+                  required
+                  value={this.state.trip}
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="col-md-4">
+          <div className="panel panel-default">
+            <FormErrors formErrors={this.state.formErrors} />
           </div>
         </div>
-        <div className="form-row p-2">
-          <div className="input-group">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="inputGroupPrepend2">
-                £/ppl
-              </span>
-            </div>
-            <input
-              type="text"
-              className="form-control"
-              id="validationDefaultUsername"
-              placeholder="£ Pence Per Litre"
-              aria-describedby="inputGroupPrepend2"
-              required
-            />
-          </div>
-        </div>
-        <div className="form-row p-2">
-          <div className="input-group">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="inputGroupPrepend2">
-                £
-              </span>
-            </div>
-            <input
-              type="text"
-              className="form-control"
-              id="validationDefaultUsername"
-              placeholder="£"
-              aria-describedby="inputGroupPrepend2"
-              required
-            />
-          </div>
-        </div>
-        <div className="form-row p-2">
-          <div className="input-group">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="inputGroupPrepend2">
-                mileage
-              </span>
-            </div>
-            <input
-              type="text"
-              className="form-control"
-              id="validationDefaultUsername"
-              placeholder="mileage"
-              aria-describedby="inputGroupPrepend2"
-              required
-            />
-          </div>
-        </div>
-        <div className="form-row p-2">
-          <div className="input-group">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="inputGroupPrepend2">
-                Trip
-              </span>
-            </div>
-            <input
-              type="text"
-              className="form-control"
-              id="validationDefaultUsername"
-              placeholder="trip"
-              aria-describedby="inputGroupPrepend2"
-              required
-            />
-          </div>
-        </div>
-      </form>
+      </div>
     );
+  }
+
+  handleUserInput(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ [name]: value }, () => {
+      this.validateField(name, value);
+    });
+  }
+
+  validateField(fieldName, value) {
+    let fieldValidationErrors = this.state.formErrors;
+    let emailValid = this.state.emailValid;
+    let passwordValid = this.state.passwordValid;
+
+    switch (fieldName) {
+      case "date":
+        emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+        fieldValidationErrors.email = emailValid ? "" : " is invalid";
+        break;
+      case "password":
+        passwordValid = value.length >= 6;
+        fieldValidationErrors.password = passwordValid ? "" : " is too short";
+        break;
+      default:
+        break;
+    }
+    this.setState(
+      {
+        formErrors: fieldValidationErrors,
+        emailValid: emailValid,
+        passwordValid: passwordValid
+      },
+      this.validateForm
+    );
+  }
+
+  validateForm() {
+    this.setState({
+      formValid: this.state.emailValid && this.state.passwordValid
+    });
   }
 }
 
